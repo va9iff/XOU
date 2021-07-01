0829b180a68aa36f5258093c7731527076994497
va9iff <va9iff@gmail.com>
2021-07-02 00:37:34 +0400

    unnecessary loop events put outside of loop

--- app.js
+++ app.js
@@ -181,13 +181,13 @@ function checurr(takenP) {
         iswining.forEach((winnerP) => {
           winnerP.style.backgroundColor = "black";
           winnerP.innerHTML = currentPlayerName() + " won";
-          stats.innerHTML = winnerP.innerHTML + " the game";
-          startbutton.style.visibility = "visible";
-          startmenu.style.display = "flex";
-          winnerplayertext.innerHTML = currentPlayerName();
-          winpopupwrapper.style.top = 0;
-          windir = [];
         });
+        stats.innerHTML = winnerP.innerHTML + " the game";
+        startbutton.style.visibility = "visible";
+        startmenu.style.display = "flex";
+        winnerplayertext.innerHTML = currentPlayerName();
+        winpopupwrapper.style.top = 0;
+        windir = [];
       }
       // everydir.forEach(() => {});
       // alldir.push(everydir);
